84f8273b39fb57ececd5a409607f137d57713c7f
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2018-12-21 11:58:50 +0000

    Update Lambda_Intro.java

--- Lambda_Intro.java
+++ Lambda_Intro.java
@@ -1,4 +1,5 @@
 import java.util.*;
+// import java.util.function;
 
 class Animal {
   private String type;
@@ -25,7 +26,7 @@ class Animal {
 }
 
 // Create interface for easy implementation
-interface CheckAnimal {
+interface CheckAnimal { // functional interface, can be used with functions
   boolean check(Animal animal);
 }
 
@@ -37,6 +38,10 @@ class CheckCanJump implements CheckAnimal {
   }
 }
 
+interface AnotherCheck {
+  boolean check(Animal first, Animal second);
+}
+
 class Main {
   public static void main(String[] args) {
     List<Animal> animals = new ArrayList<>();
@@ -44,15 +49,50 @@ class Main {
     animals.add(new Animal("rabbit", true, false));
     animals.add(new Animal("dog", true, true));
 
-    print(animals, new CheckCanJump());
+    // Fails to compile when using Predicate
+    // print(animals, new CheckCanJump()); 
 
+    // Single statement lambda expressions, does not require block of code
     print(animals, animal -> animal.canSwim()); // animals that can swim
     print(animals, animal -> !animal.canSwim()); // animals that cannot swim
+
+    print(animals, (Animal animal) -> {
+      return animal.canJump(); // return is optional for one statement
+    });
+
+    print(animals, (Animal animal) -> {
+      // Animal animal = new Animal("cat", true, false);
+      System.out.println("checking animal= " + animal.getType());
+      return animal.canJump();  // return is required for more than one statement
+    });
+
+    Animal fish = animals.get(0);
+    Animal rabbit = animals.get(1);
+    Animal dog = animals.get(2);
+
+    print(fish, rabbit, (f, s) -> f.canJump() && s.canSwim()); // multiple parameters require brackets
+    print(fish, dog, ((first, second) -> first.canSwim() && second.canSwim()));
+
+    List<String> names = new ArrayList<>();
+    names.add("John");
+    names.add("Anthony");
+    names.add("Jimmy");
+    names.add("Timmy");
+
+    System.out.println("names= " + names);
+
+    // .removeIf accepts Predicate.  
+    names.removeIf(name -> name.charAt(0) == 'J'); // == is used for primitive variables
+    System.out.println("after filter names= " + names);
   }
 
-  private static void print(List<Animal> animals, CheckAnimal filter) {
+  private static void print(Animal first, Animal second, AnotherCheck check) {
+    System.out.println(check.check(first, second));
+  }
+
+  private static void print(List<Animal> animals, Predicate<Animal> filter) {
     for(Animal animal: animals) {
-      if(filter.check(animal)) {
+      if(filter.test(animal)) { // for Predicate .test is used in place of .check
         System.out.println(animal.getType());
       }
     }
